6b6c2f5c3f6891a42058179f183f7e4886c77e91
PamelaNicks <pamelanicks@yahoo.com>
2024-07-18 10:43:16 -0500

    added on exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            MadLib();
+            //MadLib();
+
+            int added = Add(2, 2);
+            Console.WriteLine(added);
+
+            int subtracted = Subtract(10, 5);
+            Console.WriteLine(subtracted);
+
+            int multiplied = Multiply(2, 5);
+            Console.WriteLine(multiplied);
+
+            int divided = Divide(12, 6);
+            Console.WriteLine(divided);
+
+
         }
         public static void MadLib()
         {
@@ -19,5 +33,26 @@
 
             Console.WriteLine($" {userName} walked into a bar and sat by a/an {favAnimal}. A fight eruped because the {favAnimal} was wearing a {favColor} hat, which is a forbidden color in the bar. The {favColor} hat was destroyed in the brawl, but {userName} had a spare hat for the {favAnimal}. The bar patrons raised their glasses and sang along to a song on the jukebox by {favBand}. Cheers!");
         }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo) 
+        { 
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
     }
 }
